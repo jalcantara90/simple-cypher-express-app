b2c5d50d88da9621cf7d9aef7f7a951f99c0b50c
jalcantara90 <jonathan.alcantara@thetrainline.com>
2023-01-10 15:36:28 +0100

    example application

---
+++ README.md
@@ -0,0 +1,59 @@
+# Install dependencies
+```bash
+ yarn
+```
+or 
+```bash
+npm install
+```
+
+# Run server 
+```bash
+yarn start
+```
+or
+```bash
+npm start
+```
+> When we run this command we will run the server in PORT 4202
+
+## Endpoints
+In this sample application are two endpoints, /encrypt and /decrypt, both are post methods.
+
+### /encrypt
+As body of the post http call we can send any object and the response is the encrypted object as hex string.
+
+```json
+
+// example http POST /encrypt body:
+{
+   "property": "with data",
+   "nestedProperty": {
+     "key": "value"
+   }
+}
+
+// response 
+{
+  "encrypted": "242b312efd8879f3c354137a5d253517475b066eff43e2bee0a106ec1ea4de271b74db34057620c420be79401365494aca514e924dcd2fe84120700d1140f42d"
+}
+```
+
+### /decrypt
+Send the encrypted hex string as an object with the property toBeDecrypted
+```json
+// example:
+
+// example http POST /decrypt body:
+{
+    "toBeDecrypted": "242b312efd8879f3c354137a5d253517475b066eff43e2bee0a106ec1ea4de271b74db34057620c420be79401365494aca514e924dcd2fe84120700d1140f42d"
+}
+
+// response 
+{
+   "property": "with data",
+   "nestedProperty": {
+     "key": "value"
+   }
+}
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const crypto = require('node:crypto');
+const bodyParser = require('body-parser');
+
+const algorithm = 'aes-256-cbc';
+const INIT_VECTOR_KEY = 'P2B0rTWYuncCncBx';
+const ENCRYPTED_KEY = 'JxfmVQ8Ejn9ilVUL4cnDCEfDcTg0U3vB';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 4202;
+
+app.post('/encrypt', (req, res) => {
+  const cipher = crypto.createCipheriv(
+    algorithm,
+    Buffer.from(ENCRYPTED_KEY, 'utf-8'),
+    Buffer.from(INIT_VECTOR_KEY, 'utf-8')
+  );
+
+  let encrypted = cipher.update(JSON.stringify(req.body), 'utf-8', 'hex');
+
+  encrypted += cipher.final('hex');
+
+  console.log('Encrypted message: ' + encrypted);
+
+  res.send({ encrypted });
+});
+
+app.post('/decrypt', (req, res) => {
+  const { toBeDecrypted } = req.body;
+
+  const decipher = crypto.createDecipheriv(
+    algorithm,
+    Buffer.from(ENCRYPTED_KEY, 'utf-8'),
+    Buffer.from(INIT_VECTOR_KEY, 'utf-8')
+  );
+
+  let decrypted = decipher.update(toBeDecrypted, 'hex', 'utf-8');
+
+  decrypted += decipher.final('utf8');
+
+  res.send(decrypted);
+});
+
+app.listen(port, () => {
+  console.log(`app running on ${port}`);
+});
